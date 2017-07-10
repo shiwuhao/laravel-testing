d40479859f146fc3dffab641af0f4e05a1579d68
shiwuhao <shiwuhao@shiwuhao.com>
2017-07-10 08:15:45 +0800

    all

--- app/Team.php
+++ app/Team.php
@@ -4,31 +4,87 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class Team
+ * @package App
+ */
 class Team extends Model
 {
+    /**
+     * @var array
+     */
     protected $fillable = ['name'];
 
-    public function add($team)
+    /**
+     * @param $user
+     * @return mixed
+     */
+    public function add($users)
     {
-        $this->isTooManyMembers();
+        $this->isTooManyMembers($users);
 
-        return $this->member()->save($team);
+        $method = $users instanceof User ? 'save' : 'saveMany';
+
+        return $this->member()->$method($users);
     }
 
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
     public function member()
     {
         return $this->hasMany(User::class);
     }
 
+    /**
+     * @return mixed
+     */
     public function count()
     {
         return $this->member()->count();
     }
 
-    public function isTooManyMembers()
+
+    /**
+     * @param $users
+     * @throws \Exception
+     */
+    public function isTooManyMembers($users)
     {
-        if ($this->count() >= $this->size) {
+        $newTeamCount = $this->count() + count($users);
+
+        if ($newTeamCount > $this->size) {
             throw new \Exception('Two Many Members');
         }
+    }
+
+    /**
+     * @param $users
+     * @return bool|mixed
+     */
+    public function remove($users)
+    {
+        if ($users instanceof User) {
+            return $users->update(['team_id' => null]);
+        }
+
+        return $this->removeMany($users);
+    }
+
+    /**
+     * @param $users
+     * @return mixed
+     */
+    public function removeMany($users)
+    {
+        return $this->member()->whereIn('id', $users->pluck('id'))->update(['team_id' => null]);
+    }
+
+    /**
+     * @return int
+     */
+    public function resize()
+    {
+        return $this->member()->update(['team_id' => null]);
     }
 }
--- tests/Feature/TeamTest.php
+++ tests/Feature/TeamTest.php
@@ -54,8 +54,64 @@ class TeamTest extends TestCase
 
         $this->expectException('Exception');
         $team->add($userThree);
+    }
 
+    /**
+     * @test
+     */
+    public function a_team_can_remove_a_member()
+    {
+        $team = factory(Team::class)->create(['size' => 5]);
+        $users = factory(User::class, 2)->create();
 
+        $team->add($users);
 
+        $team->remove($users->first());
+
+        $this->assertEquals(1, $team->count());
+    }
+    
+    /**
+     * @test
+     */
+    public function a_team_can_remove_more_than_on_user_at_once()
+    {
+        $team = factory(Team::class)->create(['size' => 5]);
+        $users = factory(User::class, 3)->create();
+
+        $team->add($users);
+
+        $team->remove($users->take(2));
+
+        $this->assertEquals(1, $team->count());
+    }
+
+    /**
+     * @test
+     */
+    public function a_team_can_remove_all_member_at_once()
+    {
+        $team = factory(Team::class)->create(['size' => 5]);
+        $users = factory(User::class, 2)->create();
+
+        $team->add($users);
+
+        $team->resize();
+
+        $this->assertEquals(0, $team->count());
+    }
+    
+    /**
+     * @test
+     */
+    public function fix_bug_on_a_team_add_more_than_one_user_at_once()
+    {
+        $team = factory(Team::class)->create(['size' => 3]);
+
+        $users = factory(User::class, 5)->create();
+
+        $this->expectException('Exception');
+
+        $team->add($users);
     }
 }
